5986b8b262ce83aab730c1d6ee8d90e77ac08c30
shiverrrai <shivwadwani@gmail.com>
2023-09-11 08:15:17 -0700

    Initial Commit

---
+++ EveryRepCounts/EveryRepCountsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryRepCountsApp.swift
+//  EveryRepCounts
+//
+//  Created by Shiv Wadwani on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EveryRepCountsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
