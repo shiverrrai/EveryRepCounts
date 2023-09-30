5986beba8261ce4dc3f54393fb0335674bcdde45
shiverrrai <shivwadwani@gmail.com>
2023-09-30 13:44:38 -0700

    implemented sample data set along with data loading infrastructure

---
+++ EveryRepCounts/Model/ModelData.swift
@@ -0,0 +1,35 @@
+//
+//  ModelData.swift
+//  EveryRepCounts
+//
+//  Created by Shiv Wadwani on 9/30/23.
+//
+
+import Foundation
+import Combine
+
+final class ModelData: ObservableObject {
+    @Published var workouts: [Workout] = load("workoutData.json")
+}
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
