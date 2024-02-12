e410db01d831b094bfd5f5b79014489697627ffd
shiverrrai <shivwadwani@gmail.com>
2024-02-11 16:23:25 -0800

    Fix for issue #3. Changes button style for adding an exercise when pressed.

--- EveryRepCounts/Views/AddExerciseView.swift
+++ EveryRepCounts/Views/AddExerciseView.swift
@@ -8,28 +8,31 @@
 import SwiftUI
 import SwiftData
 
-struct AddExerciseView: View {
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(5)
+            .background(configuration.isPressed ? Color.white : Color.blue)
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
+            .animation(.easeOut(duration: 0.5), value: configuration.isPressed)
+    }
+}
+
+struct ExerciseRow: View {
     @Environment(\.modelContext) var modelContext
     @Bindable var workout: WorkoutModel
-    @State private var searchText = ""
-    let exerciseList = ["Bench Press", "Lat Pulldown", "Lateral Raises", "Squat"]
-    
+    var exerciseName: String
+
     var body: some View {
-        List {
-            HStack {
-                Label("Icon", systemImage: "magnifyingglass").labelStyle(.iconOnly)
-                TextField("Search for an exercise", text: $searchText)
+        HStack {
+            Text(exerciseName)
+            Spacer()
+            Button("Add Exercise", systemImage: "plus.square.fill") {
+                addExercise(exerciseName: exerciseName)
             }
-            Section(header: Text("Exercises")) {
-                ForEach(searchResults, id: \.self) { exercise in
-                    HStack {
-                        Text(exercise)
-                        Spacer()
-                        Button("Add Exercise", systemImage: "plus.square.fill", action: {addExercise(exerciseName: exercise)}).labelStyle(.iconOnly)
-                    }
-                }
-            }
-            
+            .labelStyle(.iconOnly)
+            .buttonStyle(CustomButtonStyle())
         }
     }
     
@@ -45,6 +48,41 @@ struct AddExerciseView: View {
         modelContext.insert(exercise)
         modelContext.insert(set)
     }
+}
+
+struct AddExerciseView: View {
+    @Environment(\.modelContext) var modelContext
+    @Bindable var workout: WorkoutModel
+    @State private var searchText = ""
+    let exerciseList = ["Bench Press", "Lat Pulldown", "Lateral Raises", "Squat"]
+    
+    var body: some View {
+        List {
+            HStack {
+                Label("Icon", systemImage: "magnifyingglass").labelStyle(.iconOnly)
+                TextField("Search for an exercise", text: $searchText)
+            }
+            Section(header: Text("Exercises")) {
+                ForEach(searchResults, id: \.self) { exercise in
+                    ExerciseRow(workout: workout, exerciseName: exercise)
+                }
+            }
+            
+        }
+    }
+    
+//    func addExercise(exerciseName: String) {
+//        let exercise = ExerciseModel(workoutNumber: workout.number, number: 0, name: exerciseName, timestamp: Date.now)
+//        if let lastExercise = workout.exercises.max(by: {$0.number < $1.number}) {
+//            exercise.number = lastExercise.number+1
+//        }
+//        let set = SetModel(workoutNumber: exercise.workoutNumber, exerciseNumber: exercise.number, number: 0, reps: 0, weight: 0.0, timestamp: Date.now)
+//        workout.exercises.append(exercise)
+//        exercise.workout = workout
+//        exercise.sets.append(set)
+//        modelContext.insert(exercise)
+//        modelContext.insert(set)
+//    }
     
     var searchResults: [String] {
         if searchText.isEmpty {
